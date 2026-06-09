add5771e3630e474ea0ba41586ce270feb692c38
esme415413 <es442153@gmail.com>
2026-06-09 07:53:31 -0500

    script.js

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ btnReportar.addEventListener('click', () => {
 btnManual.addEventListener('click', () => {
     alert("¿Estás seguro de que quieres leer las 50 páginas del manual?");
 });
+// 1. Seleccionamos el botón y el menú nav
+const botonMenu = document.querySelector('#boton-menu');
+const menuNavegacion = document.querySelector('#navegacion');
+
+// 2. Escuchamos el clic en el botón hamburguesa
+botonMenu.addEventListener('click', () => {
+    // 3. El toggle quita o pone la clase "menu-oculto" dinámicamente
+    menuNavegacion.classList.toggle('menu-oculto');
+});
